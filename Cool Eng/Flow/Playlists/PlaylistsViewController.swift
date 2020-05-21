16a01369f0ab98948c794e43014136f3c5eace3f
Ivan Chernetskiy <ivan.ch@gmail.com>
2020-05-21 15:43:19 +0300

    removed advert

--- Cool Eng/Flow/Playlists/PlaylistsViewController.swift
+++ Cool Eng/Flow/Playlists/PlaylistsViewController.swift
@@ -7,27 +7,20 @@
 //
 
 import UIKit
-import GoogleMobileAds
 import Firebase
 import Kingfisher
 import AVFoundation
 
 
-class PlaylistsViewController: UIViewController, PlaylistDelegate, GADRewardedAdDelegate {
+class PlaylistsViewController: UIViewController, PlaylistDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     var viewModel: PlaylistViewModel!
     
-    var rewardedAd: GADRewardedAd?
-    
-    var canShowAd = false
-    
     var selectedCell: PlaylistTableViewCell?
     
     var selectedPlaylistId: Int = 0
-    
-    var showAdvert = false
     
     var player: AVPlayer?
     var playerItem:AVPlayerItem?
@@ -36,13 +29,8 @@ class PlaylistsViewController: UIViewController, PlaylistDelegate, GADRewardedAd
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         viewModel = PlaylistViewModel(delegate: self)
-        
         setupViews()
-        setupAd()
-        
-        tableView.reloadData()
     }
     
     
@@ -63,10 +51,6 @@ class PlaylistsViewController: UIViewController, PlaylistDelegate, GADRewardedAd
         self.navigationController?.navigationItem.largeTitleDisplayMode = .never
         title = "Playlists"
         
-//        let search = UISearchController(searchResultsController: nil)
-//        search.searchResultsUpdater = self
-//        self.navigationItem.searchController = search
-        
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -77,60 +61,9 @@ class PlaylistsViewController: UIViewController, PlaylistDelegate, GADRewardedAd
         docRef.updateData(["\(Int.random(in: 0...1000000))":"\(error)"])
     }
     
-    private func setupAd() {
-        rewardedAd = GADRewardedAd(adUnitID: "ca-app-pub-9391157593798156/5256111514")
-        rewardedAd?.load(GADRequest()) { error in
-            if let error = error {
-                self.upload(error: error.localizedDescription)
-                print(error)
-            } else {
-                if self.showAdvert {
-                    self.showAd()
-                }
-                self.canShowAd = true
-            }
-        }
-    }
-    
     
     func reloadData() {
         tableView.reloadData()
-    }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: "To allow education to remain free, we have to show you ads.", message: "", preferredStyle: .alert)
-
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
-            self.showAd()
-        }))
-        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-
-        self.present(alert, animated: true)
-    }
-    
-    
-    @IBAction func showAd() {
-        showAdvert = true
-        if !canShowAd {
-            selectedCell?.activitiIndicator.startAnimating()
-        } else {
-            selectedCell?.activitiIndicator.stopAnimating()
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 7, execute: {
-            self.selectedCell?.activitiIndicator.stopAnimating()
-            self.showAdvert = false
-        })
-        
-        rewardedAd?.present(fromRootViewController: self, delegate: self)
-    }
-    
-    func rewardedAd(_ rewardedAd: GADRewardedAd, userDidEarn reward: GADAdReward) {
-        print("Reward received with currency: \(reward.type), amount \(reward.amount).")
-        PlaylistsCacheHelper.shared.addNewPlaylist(playlist: "\(selectedPlaylistId)")
-        PlaylistFirebaseHelper.shared.increaseViews(playlist: selectedPlaylistId)
-        let vc = storyboard?.instantiateViewController(identifier: "VideosListViewController") as! VideosListViewController
-        vc.playlistId = selectedPlaylistId
-        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
@@ -158,14 +91,6 @@ extension PlaylistsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedCell = tableView.cellForRow(at: indexPath) as! PlaylistTableViewCell
         selectedPlaylistId = viewModel.playlists[indexPath.row].id
-        let id = viewModel.playlists[indexPath.row].id
-//        if !PlaylistsCacheHelper.shared.getPlaylistsId().contains("\(id)") {
-//            showAlert()
-//        } else {
-//            let vc = storyboard?.instantiateViewController(identifier: "VideosListViewController") as! VideosListViewController
-//            vc.playlistId = selectedPlaylistId
-//            navigationController?.pushViewController(vc, animated: true)
-//        }
         let vc = storyboard?.instantiateViewController(identifier: "VideosListViewController") as! VideosListViewController
         vc.playlistId = selectedPlaylistId
         navigationController?.pushViewController(vc, animated: true)
